c93f0c5d84c281b32b59d44bc0e25f1052c98657
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-12-29 01:24:57 -0500

    Addressed cookie overflow error

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,13 @@ class User < ActiveRecord::Base
     end
     user
   end    
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
 end
