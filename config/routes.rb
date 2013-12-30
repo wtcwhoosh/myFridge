e9d78cdee88fa1048edb9990ca2a60889814230b
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-12-30 17:53:20 -0500

    Removed Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 MyFridge::Application.routes.draw do
- 
-
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
-  
-  devise_for :users
+   
 
   resources :recipe_loopers 
    
