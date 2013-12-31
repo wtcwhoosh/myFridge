3aa2be77df540b84419604b63084c06f45ca1e1c
Newbsylberry <chris.s.mccarthy@gmail.com>
2013-12-31 10:59:07 -0500

    Created capistrano migration

---
+++ db/migrate/20131231155719_create_users_table.rb
@@ -0,0 +1,24 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users_tables do |t|
+      t.string   "email",                  default: "", null: false
+      t.string   "encrypted_password",     default: "", null: false
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",          default: 0,  null: false
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string   "provider"
+      t.string   "uid"
+      t.string   "name"
+    end
+
+
+  end
+end
+
