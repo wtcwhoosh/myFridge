9704ea22ab890031111b50ad253943c58009222f
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-01-11 10:43:48 -0500

    Removed paperclip error

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
   
-  has_attached_file :profilePicture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
-                    :url => "/system/:attachment/:id/:style/:filename"
+  # has_attached_file :profilePicture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+  #                  :path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
+  #                  :url => "/system/:attachment/:id/:style/:filename"
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
