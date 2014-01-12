14bfdd938b0325ae50a79d3d4b6284020cd4a4dc
Newbsylberry <chris.s.mccarthy@gmail.com>
2014-01-11 22:31:37 -0500

    Added smaller picture sizes

--- app/uploaders/profile_picture_uploader.rb
+++ app/uploaders/profile_picture_uploader.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 class ProfilePictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,22 +26,26 @@ class ProfilePictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fit => [800, 800]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+   version :thumb do
+     process :resize_to_fill => [200, 200]
+   end
+
+  version :small_thumb, :from_version => :thumb do
+    process :resize_to_fill => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
